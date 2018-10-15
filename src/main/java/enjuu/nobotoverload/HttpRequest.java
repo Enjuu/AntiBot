2e4bd9182f526eebd67b65f75a90e6df49072a55
Mempler <me@mempler.de>
2018-10-15 15:27:14 +0200

    Cleaning up his shit.

--- src/main/java/enjuu/nobotoverload/HttpRequest.java
+++ src/main/java/enjuu/nobotoverload/HttpRequest.java
@@ -11,58 +11,65 @@ import java.nio.charset.StandardCharsets;
 import javax.net.ssl.HttpsURLConnection;
 
 public class HttpRequest {
-	
-	public static String getHttpString(String server) throws Exception {
-	     URL objurl = new URL(server);
-	     HttpsURLConnection connect = (HttpsURLConnection) objurl.openConnection();
-	     connect.setRequestMethod("GET");
-	     connect.setRequestProperty("User-Agent", "Mozilla/5.0");
-	     BufferedReader in = new BufferedReader(
-	             new InputStreamReader(connect.getInputStream()));
-	     String inputLine;
-	     StringBuffer response = new StringBuffer();
-	     while ((inputLine = in.readLine()) != null) {
-	     	response.append(inputLine);
-	     }
-	     in.close();
-		return response.toString();
-	}
-	
-	public static void sendOfflineWebhook() throws Exception {
-		URL url = new URL(Config.getString("webhook"));
-		URLConnection con = url.openConnection();
-		HttpURLConnection http = (HttpURLConnection)con;
-		http.setRequestMethod("POST");
-		http.setDoOutput(true);
-		http.setRequestProperty("User-Agent", "Mozilla/5.0");
-		byte[] out = "{\"username\":\"AntiBot - Status\",\"avatar_url\":\"https://www.iconfinder.com/icons/2400508/download/png/256\",\"content\":\"Registrations are disabled for 10 minutes because botting\"}" .getBytes(StandardCharsets.UTF_8);
-		int length = out.length;
+    public static String getHttpString(String server) throws Exception {
+        String inputLine;
+        URL url = new URL(server);
+        HttpsURLConnection connect = (HttpsURLConnection) url.openConnection();
 
-		http.setFixedLengthStreamingMode(length);
-		http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-		http.connect();
-		try(OutputStream os = http.getOutputStream()) {
-		    os.write(out);
-		}
-		http.getInputStream();
-	}
-	
-	public static void sendOnlineWebhook() throws Exception {
-		URL url = new URL(Config.getString("webhook"));
-		URLConnection con = url.openConnection();
-		HttpURLConnection http = (HttpURLConnection)con;
-		http.setRequestMethod("POST");
-		http.setDoOutput(true);
-		http.setRequestProperty("User-Agent", "Mozilla/5.0");
-		byte[] out = "{\"username\":\"AntiBot - Status\",\"avatar_url\":\"https://www.iconfinder.com/icons/2400508/download/png/256\",\"content\":\"Registrations are enabled back\"}" .getBytes(StandardCharsets.UTF_8);
-		int length = out.length;
+        connect.setRequestMethod("GET");
+        connect.setRequestProperty("User-Agent", "Mozilla/5.0");
 
-		http.setFixedLengthStreamingMode(length);
-		http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-		http.connect();
-		try(OutputStream os = http.getOutputStream()) {
-		    os.write(out);
-		}
-		http.getInputStream();
-	}
+        BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+
+    public static void sendOfflineWebhook() throws Exception {
+        URL url = new URL(Config.getString("webhook"));
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection) con;
+
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        http.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        byte[] out = "{\"username\":\"AntiBot - Status\",\"avatar_url\":\"https://www.iconfinder.com/icons/2400508/download/png/256\",\"content\":\"Registrations are disabled for 10 minutes because botting\"}".getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+
+        http.setFixedLengthStreamingMode(length);
+        http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        http.connect();
+
+        try (OutputStream os = http.getOutputStream()) {
+            os.write(out);
+        }
+        http.getInputStream();
+    }
+
+    public static void sendOnlineWebhook() throws Exception {
+        URL url = new URL(Config.getString("webhook"));
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection) con;
+
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        http.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        byte[] out = "{\"username\":\"AntiBot - Status\",\"avatar_url\":\"https://www.iconfinder.com/icons/2400508/download/png/256\",\"content\":\"Registrations are enabled back\"}".getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+
+        http.setFixedLengthStreamingMode(length);
+        http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        http.connect();
+
+        try (OutputStream os = http.getOutputStream()) {
+            os.write(out);
+        }
+        http.getInputStream();
+    }
 }
